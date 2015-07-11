67a19d4cafeae79cfb1c87da82f11283d66acfad
Arvind Kumar C <arvindkumar.c@gmail.com>
2015-07-12 02:12:09 +0530

    Show error in ui in case if the response throws error.

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,7 +2,9 @@ class OffersController < ApplicationController
   def index
     unless params[:uid].nil?
       fyber = Fyber.new
-      @offers = fyber.get_offers(params[:uid], params[:pub0], params[:page])['offers']
+      fyber_get_offers = fyber.get_offers(params[:uid], params[:pub0], params[:page])
+      @offers = fyber_get_offers.response['offers']
+      @errors = fyber_get_offers.errors
     end
   end
 end
