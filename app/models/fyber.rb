7a02003958d762ddb86a971e961e5da4fd603b5e
Arvind Kumar C <arvindkumar.c@gmail.com>
2015-07-11 23:43:03 +0530

    Unit tests added to validate the request generation logic.

--- app/models/fyber.rb
+++ app/models/fyber.rb
@@ -1,6 +1,37 @@
+require 'net/http'
+require 'cgi'
+
 class Fyber
+  API_KEY = 'b07a12df7d52e6c118e5d47d3f9e60135b109a1f'
+
   def get_offers
-    HTTParty.get 'http://www.google.com', {query: {q: 'abc', output: 'json'}}
-    %w(a b)
+
+    params = {appid: 157,
+              uid: 'player1',
+              format: 'json',
+              device_id: '2b6f0cc904d137be2e1730235f5664094b83',
+              locale: 'de',
+              ip: '109.235.143.113',
+              offer_types: 112,
+              timestamp: DateTime.now.to_i}
+
+    http_get('api.sponsorpay.com', '/feed/v1/offers.json', add_signature(query_params(params)))
+  end
+
+  def query_params(params)
+    params.
+        reject { |key, value| value.nil? }.
+        sort.
+        map { |key, value| "#{key}=#{value}" }.
+        join('&')
+  end
+
+  def add_signature(params)
+    hash = Digest::SHA1.hexdigest(params+ '&' + API_KEY)
+    params.concat('&hashkey=').concat(hash)
+  end
+
+  def http_get(domain, path, params)
+    Net::HTTP.get(domain, "#{path}?".concat(params))
   end
 end
--- spec/fyber_spec.rb
+++ spec/fyber_spec.rb
@@ -6,9 +6,27 @@ describe 'fyber' do
     fyber = Fyber.new
     results = %w(a b)
 
-    expect(HTTParty).to receive(:get).with('http://www.google.com',
-             :query => { :q => 'abc', :output => 'json' }).and_return(results)
+    expect(DateTime).to receive('now').and_return(DateTime.new(2015, 10, 10, 1, 1, 1).to_datetime)
+    expect(Net::HTTP).to receive('get').with('api.sponsorpay.com', '/feed/v1/offers.json?appid=157&device_id=2b6f0cc904d137be2e1730235f5664094b83&format=json&ip=109.235.143.113&locale=de&offer_types=112&timestamp=1444438861&uid=player1&hashkey=f50d9f619009c625cb720c2ffc4862ee865309e7').and_return(results)
 
     expect(fyber.get_offers).to match_array(results)
+  end
+
+  it "add_signature should add API and SHA1" do
+    fyber = Fyber.new
+    signed_value = fyber.add_signature("api=123&test=456")
+    expect(signed_value).to eq("api=123&test=456&hashkey=fe9cf82f51d920b31f0e2311c749f23aff87a167")
+  end
+
+  it "should order the parameters before signing" do
+    fyber = Fyber.new
+    expect(fyber.query_params({test: false, api_key: "some_api_key", nothing: 'ASLKJASD', int_value: 123})).
+        to eq("api_key=some_api_key&int_value=123&nothing=ASLKJASD&test=false")
+  end
+
+  it "should ignore nil values while ordering the params" do
+    fyber = Fyber.new
+    expect(fyber.query_params({test: false, api_key: "some_api_key", nothing: 'ASLKJASD', int_value: 123, nil_param: nil})).
+        to eq("api_key=some_api_key&int_value=123&nothing=ASLKJASD&test=false")
   end
 end
