d1b703a0fefb5b598080e7acecd2bf11b579747e
Arvind Kumar C <arvindkumar.c@gmail.com>
2015-07-12 00:59:31 +0530

    Rendered the offers displayed.

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,6 +4,7 @@ class OffersController < ApplicationController
       fyber = Fyber.new
       offer_response = fyber.get_offers(params[:uid], params[:pub0], params[:page])
       @offers = JSON.parse(offer_response)['offers']
+      p @offers
     end
   end
 end
